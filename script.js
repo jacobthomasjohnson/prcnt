b32ae2e53ec66fa134d73703f83445eacf64cd9e
Jacob Johnson <jacobjohnson@email.com>
2022-07-21 00:03:05 -0500

    Add ability to add another activity, cloning entire div and placing below the last activity

--- script.js
+++ script.js
@@ -1,4 +1,51 @@
-const body = document.querySelector('body')
-const header = document.querySelector('.header-main')
+/*     Functions     */
 
-body.style.paddingTop = header.clientHeight + 'px'
+const bodyPadInit = () => {
+
+  const body = document.querySelector('body')
+  const header = document.querySelector('.header-main')
+
+  body.style.paddingTop = header.clientHeight + 'px'
+
+}
+
+const addActivity = () => {
+
+  const form = document.querySelector('.activity-form');
+  const activity = data.latestActivity;
+  const submit = document.querySelector('.submit');
+  const newActivity = activity.cloneNode(true);
+
+  activity.after(newActivity);
+
+}
+
+const data = {
+
+  latestActivity: document.querySelector('#activity-default')
+
+}
+
+
+
+
+
+
+
+
+
+/*     Event Listeners     */
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  bodyPadInit();
+
+});
+
+document.querySelector('.add').addEventListener('click', () => {
+
+  addActivity();
+
+  console.log('add')
+
+});
